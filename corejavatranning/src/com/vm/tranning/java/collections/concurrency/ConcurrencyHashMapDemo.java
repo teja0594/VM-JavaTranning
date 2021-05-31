89bc5b0629365a8d4ebdf01e64ed4af9d85b305d
170030594 <170030594@kluniversity.in>
2021-05-31 10:48:50 +0530

    concurrent

--- corejavatranning/src/com/vm/tranning/java/collections/concurrency/ConcurrencyHashMapDemo.java
+++ corejavatranning/src/com/vm/tranning/java/collections/concurrency/ConcurrencyHashMapDemo.java
@@ -1,5 +1,32 @@
 package com.vm.tranning.java.collections.concurrency;
 
-public class ConcurrencyHashMapDemo {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class ConcurrencyHashMapDemo {
+public static void main(String[] args) {
+	HashMap<Integer,String> hm = new HashMap<Integer, String>();
+	hm.put(1,"Teja");
+	hm.put(2,"Surya");
+	hm.put(3, "Santosh");
+	hm.put(4,"leela");
+	for(Map.Entry<Integer,String> i:hm.entrySet())
+	{
+		System.out.println(i);
+		//hm.remove(1);
+	}
+	//Concurrency-------------------------------------------------------------
+	ConcurrentHashMap<Integer,String> chm = new ConcurrentHashMap<Integer, String>();
+	chm.putAll(hm);
+	for(Map.Entry<Integer,String> i:chm.entrySet())
+	{
+		System.out.println(i);
+		chm.remove(1);
+	}
+	System.out.println(chm);
+	
+	
+	
+}
 }
